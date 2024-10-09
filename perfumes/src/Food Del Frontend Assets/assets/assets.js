4b3e35d9325f1387001c898e25d130886a93f13e
Fatima Ali <127114699+Fatimaali11@users.noreply.github.com>
2024-10-09 17:36:07 +0500

    Add files via upload

---
+++ perfumes/src/Food Del Frontend Assets/assets/assets.js
@@ -0,0 +1,339 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './Atter4.jpg'
+import menu_2 from './Atter5.jpg'
+import menu_3 from './Atter.jpg'
+import menu_4 from './Atter1.jpg'
+import menu_5 from './whitebackgAtter2.jpg'
+import menu_6 from './Atter7.jpg'
+import menu_7 from './lightbackgAtter.jpg'
+import menu_8 from './perfumgift.jpg'
+
+import food_1 from './whitebackgperfum.jpg'
+import food_2 from './Atter1.jpg'
+import food_3 from './Atter2.jpg'
+import food_4 from './Atter5.jpg'
+import food_5 from './perfum2.jpg'
+import food_6 from './Atter11.jpg'
+import food_7 from './Atter.jpg'
+import food_8 from './Atter3.jpg'
+import food_9 from './Atter5.jpg'
+import food_10 from './Atter4.jpg'
+import food_11 from './Atter8.jpg'
+import food_12 from './whitebackgperfum.jpg'
+import food_13 from './lightbackgAtter.jpg'
+import food_14 from './whitebackgperfum2.jpg'
+import food_15 from './Atter3.jpg'
+import food_16 from './whitebackgperfum3.jpg'
+import food_17 from './Atter5.jpg'
+import food_18 from './Atter9.jpg'
+import food_19 from './Atter10.jpg'
+import food_20 from './Atter7.jpg'
+import food_21 from './Atter1.jpg'
+import food_22 from './Atter3.jpg'
+
+import food_23 from './Atter2.jpg'
+import food_24 from './Atter9.jpg'
+import food_25 from './Atter.jpg'
+import food_26 from './Atter4.jpg'
+import food_27 from './Atter3.jpg'
+import food_28 from './Atter9.jpg'
+import food_29 from './Atter8.jpg'
+import food_30 from './Atter7.jpg'
+import food_31 from './Atter11.jpg'
+import food_32 from './Atter5.jpg'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import newone from './baner.jpeg' 
+
+
+export const assets = {
+    logo,
+    newone ,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    // parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Best sellers",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Mens Attars",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Womens Attars",
+        menu_image: menu_4
+    },
+   
+    {
+        menu_name: "Eastern Attar",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Western Attars",
+        menu_image: menu_8
+    },
+     {
+        menu_name: "premium bakhoor",
+        menu_image: menu_2
+    },
+]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "turkish oud ",
+        image: food_1,
+        price: 12,
+        description: "Where ancient meets modern: Turkish Oud, blending traditions",
+        category: "Eastern Attar"
+        
+    },
+    {
+        _id: "2",
+        name: "Do It Now ",
+        image: food_2,
+        price: 4,
+        description: "Do It Now is a signature fragrance for those who are all about the excitement of new love.  ",
+        category: "Eastern Attar"
+    }, {
+        _id: "3",
+        name: "Fame Legend",
+        image: food_3,
+        price: 16,
+        description: " Fame Legend Is a fruity floral fragrance, as the name suggests it is an inviting and pleasing blend. It features fruity and citrus, balanced by a woody vanilla base.",
+        category: 'Western Attars'
+    }, {
+        _id: "4",
+        name: "Ameer Al Oud ",
+        image: food_4,
+        price: 24,
+        description: " Ameer Al Oudh Bakhoor features a delicate oudh floral aroma. Unlike strong bakhoor scents, its gentle fragrance creates a soothing atmosphere for your occasions.",
+        category: "Womens Attars"
+    }, {
+        _id: "5",
+        name: "Invitation",
+        image: food_5,
+        price: 14,
+        description: "Invitation is a fragrance made for the man who enjoys warm, cozy scents. It starts with a mix of lavender, cardamom, and black pepper for a spicy yet refreshing opening. ",
+        category: "Eastern Attar"
+    }, {
+        _id: "6",
+        name: "The Great Oud",
+        image: food_6,
+        price: 9,
+        description:  "Experience the majesty of Oud, the king of fragrances",
+        category: "Western Attars"
+    }, {
+        _id: "7",
+        name: "Legacy continued",
+        image: food_7,
+        price: 10,
+        description: "Legacy Continued is a fragrance for the modern man who wants a balance between style and relaxation.    ",
+        category: "best sellers"
+    }, {
+        _id: "8",
+        name: "Be A Leader",
+        image: food_8,
+        price: 15,
+        description: "Be a Leader is designed for the man who stands out and takes charge. This fragrance starts with a refreshing mix of bergamot, black currant, apple, lemon, and pink pepper, giving it a fresh and vibrant kick.",
+        category: "Eastern Attar"
+    }, {
+        _id: "9",
+        name: "Ameer Al Oudh Bakhoor ",
+        image: food_9,
+        price: 14,
+        description: "Ameer Al Oudh Bakhoor features a delicate oudh floral aroma. Unlike strong bakhoor scents, its gentle fragrance creates a soothing atmosphere for your occasions",
+        category: "premium bakhoor"
+    }, {
+        _id: "10",
+        name: "Original Al Munakh climate",
+        image: food_10,
+        price: 22,
+        description: "Climate is a spicy fresh fragrance crafted specifically for men, with a classic touch that makes you feel refreshed and confident. It’s a universally loved aroma perfect for daily wear!",
+        category: "Western Attars"
+    }, {
+        _id: "11",
+        name: "Oud Al Aswad",
+        image: food_11,
+        price: 10,
+        description: "Oudh Al Aswad Bakhoor feels sweet, floral, and slightly spicy, with citrus and green undertones, evoking a romantic, luxurious, and natural scent reminiscent of lush, blooming gardens",
+        category: "premium bakhoor"
+    }, {
+        _id: "12",
+        name: "The great oud",
+        image: food_12,
+        price: 12,
+        description: "Oud, where wood and oil entwine, love is born",
+        category: "mans Attar"
+    },
+    {
+        _id: "13",
+        name: "Ghilaf-e-Kaaba",
+        image: food_13,
+        price: 12,
+        description: "Ghilaf-e-Kaaba Attar: Purify your senses, divine connection.",
+        category: "Womens Attars"
+    },
+    {
+        _id: "14",
+        name: "Savage Men ",
+        image: food_14,
+        price: 8,
+        description: "Savage Man is a boldly masculine fragrance with strong notes that last a whole day.",
+        category: "Mens Attars"
+    }, {
+        _id: "15",
+        name: "Be A Leader",
+
+        image: food_15,
+        price: 16,
+        description: "Be a Leader is designed for the man who stands out and takes charge. This fragrance starts with a refreshing mix of bergamot, black currant, apple, lemon, and pink pepper, giving it a fresh and vibrant kick.",
+
+        category: "Mens Attars"
+    }, {
+        _id: "16",
+        name: "Furat",
+        image: food_16,
+        price: 5,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Best sellers"
+    },{
+        _id: "4",
+        name: "Ameer Al Oud  ",
+        image: food_4,
+        price: 24,
+        description: " Ameer Al Oudh Bakhoor features a delicate oudh floral aroma. Unlike strong bakhoor scents, its gentle fragrance creates a soothing atmosphere for your occasions.",
+        category: " Womens Attars"
+    }, {
+        _id: "28",
+        name: "Hajar-E-Aswad",
+        image: food_28,
+        price: 17,
+        description: " Hajar-E-Aswad bakhoor has an amber soothing fragrance with a touch of rose - an aroma that symbolizes faith and devotion, specially crafted for religious occasions.",
+        category:"premium bakhoor"
+    },{
+        _id: "30",
+        name: "Oudh Al Ward  ",
+        image: food_30,
+        price: 12,
+        description: "Oudh Al Ward bakhoor breaks the tradition of strong bakhoors, offering a soothing aroma with its sweet oudh scent that provides a calming vibe",
+        category: "premium bakhoor"
+    }, {
+        _id: "3",
+        name: "Fame Legend",
+        image: food_3,
+        price: 16,
+        description: " Fame Legend Is a fruity floral fragrance, as the name suggests it is an inviting and pleasing blend. It features fruity and citrus, balanced by a woody vanilla base.",
+        category: 'Womens Attar'
+    },    {
+        _id: "1",
+        name: "turkish oud ",
+        image: food_1,
+        price: 12,
+        description: "Where ancient meets modern: Turkish Oud, blending traditions",
+        category: "Estern Attar"
+    },
+    {
+        _id: "2",
+        name: "Do It Now ",
+        image: food_2,
+        price: 4,
+        description: "Do It Now is a signature fragrance for those who are all about the excitement of new love.  ",
+        category: "Mens Attars"
+    }, {
+        _id: "3",
+        name: "Fame Legend",
+        image: food_3,
+        price: 16,
+        description: " Fame Legend Is a fruity floral fragrance, as the name suggests it is an inviting and pleasing blend. It features fruity and citrus, balanced by a woody vanilla base.",
+        category: 'Womens Attar'
+    }, {
+        _id: "5",
+        name: "Invitation",
+        image: food_5,
+        price: 14,
+        description: "Invitation is a fragrance made for the man who enjoys warm, cozy scents. It starts with a mix of lavender, cardamom, and black pepper for a spicy yet refreshing opening. ",
+        category: "Womens Attars"
+    }, 
+    {
+        _id: "12",
+        name: "The great oud",
+        image: food_12,
+        price: 12,
+        description: "Oud, where wood and oil entwine, love is born",
+        category: "Best sellers"
+    },
+    {
+        _id: "10",
+        name: "Original Al Munakh climate",
+        image: food_10,
+        price: 22,
+        description: "Climate is a spicy fresh fragrance crafted specifically for men, with a classic touch that makes you feel refreshed and confident. It’s a universally loved aroma perfect for daily wear!",
+        category: "Mens Attars"
+    },{
+        _id: "15",
+        name: "Be A Leader",
+
+        image: food_15,
+        price: 16,
+        description: "Be a Leader is designed for the man who stands out and takes charge. This fragrance starts with a refreshing mix of bergamot, black currant, apple, lemon, and pink pepper, giving it a fresh and vibrant kick.",
+
+        category: "Western Attars"
+    }, {
+        _id: "28",
+        name: "Hajar-E-Aswad",
+        image: food_28,
+        price: 17,
+        description: " Hajar-E-Aswad bakhoor has an amber soothing fragrance with a touch of rose - an aroma that symbolizes faith and devotion, specially crafted for religious occasions.",
+        category:"Best sellers"
+    }, {
+        _id: "29",
+        name: "Oud Al Aswad",
+        image: food_29,
+        price: 14,
+        description: "Oudh Al Aswad Bakhoor feels sweet, floral, and slightly spicy, with citrus and green undertones, evoking a romantic, luxurious, and natural scent reminiscent of lush, blooming gardens.",
+        category: "Best sellers"
+    }, {
+        _id: "30",
+        name: "Oudh Al Ward  ",
+        image: food_30,
+        price: 12,
+        description: "Oudh Al Ward bakhoor breaks the tradition of strong bakhoors, offering a soothing aroma with its sweet oudh scent that provides a calming vibe",
+        category: "Best sellers"
+    }, 
+]
